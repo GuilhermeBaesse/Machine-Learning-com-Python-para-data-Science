53ebd6aaf44afdb582af13798866f838bba63d7c
GuilhermeBaesse <guilherme.baesse@estudante.ufla.br>
2021-11-08 15:22:24 -0300

    aprofundando em como a multipla regressão funciona

---
+++ Regressão/Multiple Linear Regression/multiple_linear_regression.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#importing dataset
+dataset = pd.read_csv('50_Startups.csv ')
+X = dataset.iloc[:, :-1].values
+Y = dataset.iloc[:, -1].values
+
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[('encoder',OneHotEncoder(),[3])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
+
+#spliting in training and test
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
+
+#training multiple Linear Regression
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, Y_train)
+
+#Predicting the test set results
+y_pred = regressor.predict(X_test)
+np.set_printoptions(precision=2)
+print(np.concatenate())
